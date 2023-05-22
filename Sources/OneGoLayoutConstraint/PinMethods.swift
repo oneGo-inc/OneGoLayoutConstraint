f71dbce2e9f3de2e2355aeae79d57302770f7e51
Murat <89196242960@mail.ru>
2023-05-22 10:53:56 +0300

    Adding some function & documentation

--- Sources/OneGoLayoutConstraint/PinMethods.swift
+++ Sources/OneGoLayoutConstraint/PinMethods.swift
@@ -11,17 +11,35 @@ public extension UIView {
 
     // MARK: - Instance Methods
     
+    /// Pin view to superview
+    /// - Parameter sides: Side to pin
+    func pinToSuperView(sides: Side...) {
+        guard let view = self.superview else { return }
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.pin(sides: sides, toSameSidesOn: view)
+    }
+    
+    /// Pin view to superview
+    /// - Parameter sides: Defaul is all sides
     func pinToSuperView(sides: [Side] = Side.allSides) {
         guard let view = self.superview else { return }
         self.translatesAutoresizingMaskIntoConstraints = false
         self.pin(sides: sides, toSameSidesOn: view)
     }
-
-    func pin(side: Side, toSameSidesOn view: UIView, isActive: Bool = true) {
-        self.pin(sides: [side], toSameSidesOn: view, isActive: isActive)
+    
+    /// Pin side of some view to same side of another view
+    /// - Parameters:
+    ///   - side: Side to pin
+    ///   - view: View to which pin
+    func pin(side: Side, toSameSidesOn view: UIView) {
+        self.pin(sides: [side], toSameSidesOn: view)
     }
-
-    func pin(sides: [Side] = Side.allSides, toSameSidesOn view: UIView, isActive: Bool = true) {
+    
+    /// Pin sides of some view to same side of another view
+    /// - Parameters:
+    ///   - side: Side to pin
+    ///   - view: View to which pin
+    func pin(sides: [Side] = Side.allSides, toSameSidesOn view: UIView) {
         for side in sides {
             let layout: NSLayoutConstraint
             switch side {
@@ -44,11 +62,60 @@ public extension UIView {
                 layout = self.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: cGFloat)
                 layout.priority = uILayoutPriority
             }
-            layout.isActive = isActive
+            layout.isActive = true
         }
+    }
+    
+    @discardableResult
+    /// Pin side of some view to safe area side of another view
+    /// - Parameters:
+    ///   - side: Side to pin
+    ///   - viewSide: View to which pin
+    ///   - isActive: Is creating Constraint is active
+    /// - Returns: Created Constraint
+    func pinToSafeArea(side: Side, to viewSide: ViewSide, isActive: Bool = true) -> NSLayoutConstraint? {
+        let layout: NSLayoutConstraint?
+        switch (side, viewSide) {
+        case (.top(let constant, let uILayoutPriority), .top(let view)):
+            layout = self.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.top(let constant, let uILayoutPriority), .bottom(let view)):
+            layout = self.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.bottom(let constant, let uILayoutPriority), .top(let view)):
+            layout = self.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.bottom(let constant, let uILayoutPriority), .bottom(let view)):
+            layout = self.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.left(let constant, let uILayoutPriority), .right(let view)):
+            layout = self.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.left(let constant, let uILayoutPriority), .left(let view)):
+            layout = self.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.right(let constant, let uILayoutPriority), .right(let view)):
+            layout = self.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        case (.right(let constant, let uILayoutPriority), .left(let view)):
+            layout = self.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: constant)
+            layout?.priority = uILayoutPriority
+        default:
+            print("ERROR IN SET CONSTRAINT")
+            return nil
+        }
+        layout?.isActive = isActive
+        return layout
     }
 
     @discardableResult
+    /// Pin side of some view to side of another view
+    /// Can't pin centerY & centerX, for this use pinToSuperView
+    /// - Parameters:
+    ///   - side: Side to pin
+    ///   - viewSide: View to which pin
+    ///   - isActive: Is creating Constraint is active
+    /// - Returns: Created Constraint
     func pin(side: Side,to viewSide: ViewSide, isActive: Bool = true) -> NSLayoutConstraint? {
         let layout: NSLayoutConstraint?
         switch (side, viewSide) {
@@ -85,6 +152,11 @@ public extension UIView {
     }
 
     @discardableResult
+    /// Set height of width for some view
+    /// - Parameters:
+    ///   - demission: Height of width with constant
+    ///   - isActive: Is creating Constraint is active
+    /// - Returns: Created Constraint
     func setDemission(_ demission: Demission, isActive: Bool = true) -> NSLayoutConstraint {
         let constraint: NSLayoutConstraint
         switch demission {
@@ -96,13 +168,23 @@ public extension UIView {
         constraint.isActive = isActive
         return constraint
     }
-
+    
+    /// Set eqaul value for height and width of some view
+    /// - Parameters:
+    ///   - constant: Constant of sides
+    ///   - isActive: Is creating Constraints is active
     func setEqualSize(constant: CGFloat, isActive: Bool = true) {
         self.heightAnchor.constraint(equalToConstant: constant).isActive = isActive
         self.widthAnchor.constraint(equalTo: self.heightAnchor).isActive = isActive
     }
 
     @discardableResult
+    /// Set eqaul value for demission of some view
+    /// - Parameters:
+    ///   - demission: Height or width with constant
+    ///   - view: View to which set eqaul side constant
+    ///   - isActive:  Constant of sides
+    /// - Returns: Is creating Constraints is active
     func equal(_ demission: Demission, to view: UIView, isActive: Bool = true) -> NSLayoutConstraint{
         let constraint: NSLayoutConstraint
         switch demission {
@@ -110,6 +192,29 @@ public extension UIView {
             constraint = self.heightAnchor.constraint(equalTo: view.heightAnchor, constant: constant)
         case .width(let constant):
             constraint = self.widthAnchor.constraint(equalTo: view.widthAnchor, constant: constant)
+        }
+        constraint.isActive = isActive
+        return constraint
+    }
+    
+    @discardableResult
+    /// Set eqaul value for demission of some view
+    /// - Parameters:
+    ///   - demission: Height or width with constant
+    ///   - view: View to which set eqaul side constant
+    ///   - isActive:  Constant of sides
+    /// - Returns:  Is creating Constraints is active
+    func equal(_ demission: Demission, to view: ViewDemision, isActive: Bool = true) -> NSLayoutConstraint{
+        let constraint: NSLayoutConstraint
+        switch (demission, view) {
+        case let (.height(constant), .height(view)):
+            constraint = self.heightAnchor.constraint(equalTo: view.heightAnchor, constant: constant)
+        case let (.height(constant), .width(view)):
+            constraint = self.heightAnchor.constraint(equalTo: view.widthAnchor, constant: constant)
+        case let (.width(constant), .width(view)):
+            constraint = self.widthAnchor.constraint(equalTo: view.widthAnchor, constant: constant)
+        case let (.width(constant), .height(view)):
+            constraint = self.widthAnchor.constraint(equalTo: view.heightAnchor, constant: constant)
         }
         constraint.isActive = isActive
         return constraint
---
+++ Sources/OneGoLayoutConstraint/ViewDemision.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Мурат Камалов on 5/22/23.
+//
+
+import UIKit
+
+extension UIView {
+    
+    // MARK: - ViewDemision cases
+
+    public enum ViewDemision {
+        case height(UIView)
+        case width(UIView)
+    }
+}
